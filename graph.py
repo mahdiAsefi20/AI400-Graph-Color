f624b751ff9307279bd3adbd0cb0338f06325689
ark1375 <ark.blackhat@gmail.com>
2021-12-30 16:41:12 +0330

    Add graphe, coloring and sample graph

--- graph.py
+++ graph.py
@@ -1,7 +1,11 @@
+COLOR = ['RED' , 'BLUE' , 'WHITE' , 'YELLOW' , 'GREEN']
+
+
 class Node:
-    def __init__(self, ID , node_list = []):
+    def __init__(self, ID , node_list = [] , color = None):
         self.__id = ID
         self.__connections = node_list.copy()
+        self.__color = color
 
     def get_id(self):
         return self.__id
@@ -32,13 +36,67 @@ class Node:
 
         return False
 
+    def colorize(self , color):
+        self.__color = color
+
+    def get_color(self):
+        return self.__color
+
     def __repr__(self):
-        return self.__id
+        return  str(self.__id)
 
 
 
+class Graph:
+
+    def __init__(self, name = 'default' , node_list = []):
+        self.nodes = node_list
+        self.name = name
 
 
+    def is_coloring_valid(self):
+
+        for node in self.nodes:
+
+            my_color = node.get_color()
+
+            for neighbour in node.neighbours():
+                
+                if my_color == neighbour.get_color():
+                    return False
+
+        return True
+
+    
+    def score(self): # Fitness Function
+        # Needed for genetics Algorithm
+        pass
+
+    @staticmethod
+    def read_file(pth):
+        with open(pth , 'r') as file:
+            num_of_nodes = int( file.readline() )
+            
+            # node_list = [Node(name) for name in range(1 , num_of_nodes + 1)]    # list comperhension
+            
+            node_list = { str(name): Node(name) for name in range(1 , num_of_nodes + 1)}
+
+            for index in range(1 , num_of_nodes + 1 ):
+                file_line = file.readline()
+                current_node_list = file_line.split(' ')[:-1]
+                
+                for node_key in current_node_list:
+                    if (node_key == '-1'):
+                        break
+
+                    node_list[str(index)].create_connection(
+                        node_list[node_key]
+                    )
+
+        return Graph(node_list.values())
+
+
+# my_graph = Graph.read_file('sample-graph.gp')
 
 
 
@@ -51,8 +109,19 @@ a.create_connection(b)
 b.create_connection(c)
 c.create_connection(a)
 
+a.colorize('RED')
+b.colorize('RED')
+c.colorize('GREEN')
 
-print(a.neighbours())
-print(b.neighbours())
-print(c.neighbours())
+my_graph = Graph(node_list= [a,b,c])
+print(
+    my_graph.is_coloring_valid()
+)
 
+
+
+###### For Writing Custom File ######
+## Line 0: Number Of Node
+## Line 1 - NON: Connections to other node
+### Reminder. Note that at the end of each line from line 1 to line NON you must
+### leave a blank space
